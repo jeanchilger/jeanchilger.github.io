b2e405980f890bb9e14295cd32ae89eb0857e87d
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2021-02-07 21:19:18 -0300

    chore: changes structure to gatsby

---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const BlogPage = () => {
+    return (
+        <div>
+            <h1>Latest Blog Posts</h1>
+        </div>
+    )
+}
+
+export default BlogPage
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Jean Carlo Hilger</h1>
+            <h2>Bio comes here</h2>
+            <Link to="/blog">Blog</Link>
+        </div>
+    )
+}
+
+export default HomePage;
