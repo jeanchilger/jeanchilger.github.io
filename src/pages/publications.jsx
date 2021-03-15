0316450aac4e1b1fbe873ace7b07385a5fd09300
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2021-03-15 00:02:59 -0300

    feat: adds basic WIP pages for blog and publications

--- src/pages/publications.jsx
+++ src/pages/publications.jsx
@@ -5,7 +5,7 @@ function BlogPage() {
     return (
         <Layout>
             <div className="container">
-                <h1 className="title title--primary">Latest Blog Posts</h1>
+                <h1 className="title title--primary">Publications</h1>
                 <h2 className="subtitle subtitle--light">Work In Progress</h2>
             </div>
         </Layout>
