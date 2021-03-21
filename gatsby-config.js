83fda1a673f57e1013e3bfb3e60d0f60e6dcf4c5
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2021-03-21 03:12:34 -0300

    fix(blog): Sets right blog pages loading.

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,18 @@ module.exports = {
     /* Your site config here */
     plugins: [
         'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-plugin-mdx',
+            options: {
+                extensions: ['.mdx', '.md'],
+            },
+        },
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'posts',
+                path: `${__dirname}/src/posts`,
+            },
+        },
     ],
 };
