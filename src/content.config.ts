2335e5a6ee82d67db0167e9fd45f5c1a9e13d4a5
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2026-06-10 10:02:07 -0300

    feat: adds basic blog saving and listing strattegy

---
+++ src/content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection } from 'astro:content';
+import { glob, file } from 'astro/loaders';
+import { z } from 'astro/zod';
+
+const blog = defineCollection({
+    loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+    }),
+});
+
+
+
+export const collections = { blog };
