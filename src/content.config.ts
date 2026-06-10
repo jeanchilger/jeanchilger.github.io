6191971b7085ce9d0577b0abb6fd0fbbd6442604
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2026-06-10 18:39:37 -0300

    fix: adds missing fields in blog entry schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,6 +7,8 @@ const blog = defineCollection({
     schema: z.object({
         title: z.string(),
         description: z.string(),
+        date: z.date(),
+        tags: z.array(z.string()),
     }),
 });
 
